1dbca20554ea1465e54408ae1e5a89767d158738
HeitorDalla <heitorvillavilla@gmail.com>
2025-05-30 15:02:10 -0300

    Inserindo todos os dados

--- projeto-academia.py
+++ projeto-academia.py
@@ -6,17 +6,20 @@ import sqlite3
 import csv
 
 # 📦 Conecta (ou cria) o banco de dados SQLite
-conn = sqlite3.connect("ProjetoAcademia/academia.db", check_same_thread=False)
+conn = sqlite3.connect("gym-system.db", check_same_thread=False)
 cursor = conn.cursor()
 
 # Criação da tabela clientes (DDL)
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS clientes (
+CREATE TABLE IF NOT EXISTS clientes_academia (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL,
+    idade INTEGER NOT NULL,
+    sexo TEXT NOT NULL,
     email TEXT NOT NULL,
     telefone TEXT NOT NULL,
-    data_nascimento TEXT NOT NULL
+    plano_id INTEGER NOT NULL,
+    FOREIGN KEY(plano_id) REFERENCES planos(id)
 )
 ''')
 
@@ -57,7 +60,7 @@ CREATE TABLE IF NOT EXISTS treinos (
     data_inicio TEXT NOT NULL,
     data_fim TEXT NOT NULL,
     plano_id INTEGER NOT NULL,
-    FOREIGN KEY(cliente_id) REFERENCES clientes(id),
+    FOREIGN KEY(cliente_id) REFERENCES clientes_academia(id),
     FOREIGN KEY(instrutor_id) REFERENCES instrutores(id),
     FOREIGN KEY(plano_id) REFERENCES planos(id)
 )
@@ -65,7 +68,7 @@ CREATE TABLE IF NOT EXISTS treinos (
 
 # Criação da tabela treino_exercicio (DDL)
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS treino_exercicio (
+CREATE TABLE IF NOT EXISTS treino_exercicios (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     treino_id INTEGER NOT NULL,
     exercicio_id INTEGER NOT NULL,
@@ -78,13 +81,38 @@ CREATE TABLE IF NOT EXISTS treino_exercicio (
 
 # Criação da tabela pagamentos (DDL)
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS pagamentos (
+CREATE TABLE IF NOT EXISTS pagamento_clientes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     cliente_id INTEGER NOT NULL,
+    plano_id INTEGER NOT NULL,
+    valor_pago REAL NOT NULL,
     data_pagamento TEXT NOT NULL,
-    valor REAL NOT NULL,
-    pago BOOLEAN NOT NULL,
-    FOREIGN KEY(cliente_id) REFERENCES clientes(id)
+    FOREIGN KEY(cliente_id) REFERENCES clientes_academia(id),
+    FOREIGN KEY(plano_id) REFERENCES planos(id)
 )
 ''')
 conn.commit()
+
+
+cursor.execute('PRAGMA foreign_keys = ON;')
+
+df_exercicios = pd.read_csv('exercicios.csv')
+df_exercicios.to_sql('exercicios', conn, if_exists='append', index=False)
+
+df_exercicios = pd.read_csv('instrutores.csv')
+df_exercicios.to_sql('instrutores', conn, if_exists='append', index=False)
+
+df_exercicios = pd.read_csv('planos.csv')
+df_exercicios.to_sql('planos', conn, if_exists='append', index=False)
+
+df_exercicios = pd.read_csv('clientes_academia.csv')
+df_exercicios.to_sql('clientes_academia', conn, if_exists='append', index=False)
+
+df_exercicios = pd.read_csv('pagamento_clientes.csv')
+df_exercicios.to_sql('pagamento_clientes', conn, if_exists='append', index=False)
+
+df_exercicios = pd.read_csv('treinos.csv')
+df_exercicios.to_sql('treinos', conn, if_exists='append', index=False)
+
+df_exercicios = pd.read_csv('treino_exercicios.csv')
+df_exercicios.to_sql('treino_exercicios', conn, if_exists='append', index=False)
